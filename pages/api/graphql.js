efc77cd5fc2c9e058eec8afc1ef7ec69cf8c0427
Chandra penugonda <chandrapenugonda655@gmail.com>
2020-03-28 22:20:01 +0530

    merge graphql schemas using custom babelrc

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,19 +1,30 @@
+import { mergeTypeDefs, mergeResolvers } from "@graphql-toolkit/schema-merging";
 import { ApolloServer, gql } from "apollo-server-micro";
 import connectDB from "../../lib/mongodb";
+import habitsTypedefs from "../../api/habits/habits.graphql";
+import habitsMutationResolvers from "../../api/habits/mutation";
+import habitsQueryResolvers from "../../api/habits/query";
 
-const typeDefs = gql`
+const fakeTypeDefs = gql`
   type Query {
     sayHello: String
   }
 `;
 
-const resolvers = {
+const fakeResolvers = {
   Query: {
     sayHello() {
       return "Hello World!";
     }
   }
 };
+
+const typeDefs = mergeTypeDefs([fakeTypeDefs, habitsTypedefs]);
+const resolvers = mergeResolvers([
+  fakeResolvers,
+  habitsQueryResolvers,
+  habitsMutationResolvers
+]);
 
 export const config = {
   api: {
